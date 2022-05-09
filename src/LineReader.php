a294bfb5632c2b8870374badf2448c326bc28460
FCNO <fcno.dev@gmail.com>
2022-05-09 20:18:26 -0300

    fix(core): $file as param and not as property
    As a parameter, the reference to the file is not lost as in the case of
    treating it as a property.

--- src/LineReader.php
+++ src/LineReader.php
@@ -5,16 +5,10 @@ namespace FruiVita\LineReader;
 use FruiVita\LineReader\Contracts\IReadable;
 use FruiVita\LineReader\Exceptions\FileNotReadableException;
 use Illuminate\Pagination\LengthAwarePaginator;
+use SplFileObject;
 
 class LineReader implements IReadable
 {
-    /**
-     * File to be read.
-     *
-     * @var \SplFileObject
-     */
-    protected \SplFileObject $file;
-
     /**
      * {@inheritdoc}
      */
@@ -22,9 +16,9 @@ class LineReader implements IReadable
     {
         throw_if(! $this->isReadable($file_path), FileNotReadableException::class);
 
-        $this->setWorkingFile($file_path);
+        $file = $this->setWorkingFile($file_path);
 
-        return $this->read();
+        return $this->read($file);
     }
 
     /**
@@ -35,11 +29,11 @@ class LineReader implements IReadable
         throw_if(! $this->isReadable($file_path), FileNotReadableException::class);
         throw_if(($per_page < 1 || $page < 1), \InvalidArgumentException::class);
 
-        $this->setWorkingFile($file_path);
+        $file = $this->setWorkingFile($file_path);
 
         return new LengthAwarePaginator(
-            items: $this->readPage($per_page, $page),
-            total: $this->totalLines(),
+            items: $this->readPage($file, $per_page, $page),
+            total: $this->totalLines($file),
             perPage: $per_page,
             currentPage: $page,
             options: [
@@ -68,39 +62,45 @@ class LineReader implements IReadable
      *
      * @param string $file_path full path
      *
-     * @return void
+     * @return \SplFileObject
      */
     private function setWorkingFile(string $file_path)
     {
-        $this->file = new \SplFileObject($file_path);
+        $file = new \SplFileObject($file_path);
 
-        $this->file->setFlags(\SplFileObject::DROP_NEW_LINE);
+        $file->setFlags(\SplFileObject::DROP_NEW_LINE);
+
+        return $file;
     }
 
     /**
      * Effectively reads the file and returns the current line.
      *
+     * @param \SplFileObject $file File to be read
+     *
      * @return \Generator
      */
-    private function read()
+    private function read(SplFileObject $file)
     {
-        while (! $this->file->eof()) {
-            yield $this->file->current();
-            $this->file->next();
+        while (! $file->eof()) {
+            yield $file->current();
+            $file->next();
         }
     }
 
     /**
      * Total number of lines in the file.
      *
+     * @param \SplFileObject $file File to be read
+     *
      * @return int
      */
-    private function totalLines()
+    private function totalLines(SplFileObject $file)
     {
         // Seek for end of file
-        $this->file->seek(PHP_INT_MAX);
+        $file->seek(PHP_INT_MAX);
 
-        return $this->file->key() + 1;
+        return $file->key() + 1;
     }
 
     /**
@@ -114,18 +114,19 @@ class LineReader implements IReadable
      * - index 99 line 100
      * - and so on
      *
+     * @param \SplFileObject $file File to be read
      * @param int $per_page
      * @param int $page
      *
      * @return \Illuminate\Support\Collection
      */
-    private function readPage(int $per_page, int $page)
+    private function readPage(SplFileObject $file, int $per_page, int $page)
     {
         $offset = ($page - 1) * $per_page;
 
         $collection = collect();
 
-        $iterator = new \LimitIterator($this->read(), $offset, $per_page);
+        $iterator = new \LimitIterator($this->read($file), $offset, $per_page);
 
         foreach ($iterator as $key => $line) {
             $collection->put($key, $line);
